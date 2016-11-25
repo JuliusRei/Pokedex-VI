85db4db27b9d7150769dfb1b9fc8588a75179bdd
JuliusRei <morcillajr0912@gmail.com>
2016-11-25 16:54:45 +0800

    2nd commit, no comment area yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+
 import React from 'react';
 import head from './Pokemon.png';
 import './App.css';
-import Request from 'superagent'
-import { Button } from 'react-bootstrap';
-var Comment = React.createClass({
+import Request from 'superagent';
 
+var Comment = React.createClass({
+  
   render(){
+    
     return(<div className = "leftclass col-xs-4" >
-      <textarea className = "form-control" style = {{marginBottom: "1%"}} disabled></textarea>
-      <button className = "btn btn-info">Save</button>
+      <h1  style = {{color:"black"}}> Comment Area</h1>
+      <textarea className = "form-control" style = {{marginBottom: "1%"}}></textarea>
+      <button id = "btnsave" className = "btn btn-info" onClick= {this.handleClick} >Leave a Comment</button>
+      <div className = "col-xs-12 commentArea" style ={{color:"white"}} ></div>
       </div>);
   }
 });
@@ -20,15 +24,19 @@ var Detail = React.createClass({
   backgroundColor:"white",
   paddingTop:"3px"
 };
- var type = this.props.types;
-      if(type != ""){
-        type = this.props.types.map((n)=> {return "/"+n.type.name + "/"});
+var abilities = this.props.ability;
+var type = this.props.types;
+      if(type !== ""){
+        type = this.props.types.map((n)=> {return "/"+n.type.name + "/"}); 
+        abilities = this.props.ability.map((n)=> {return "/"+n.ability.name + "/"});
       }
         else{
-          type = "none";        }
+          type = "-"; 
+          abilities = "-";         }
 
     return(
           <div className = "leftclass col-xs-4" >
+          <h1 style = {{color:"black"}}> Pokemon Descripton</h1>
       <div className= "form-group">
       <div className = "col-xs-6" style = {border}>
       <img className = "sprite" src = {this.props.sprite}/><p>Original</p></div>
@@ -39,6 +47,7 @@ var Detail = React.createClass({
       <p>Name: {this.props.name}</p>
       <p>Weight : {this.props.weight}</p>
       <p>Type: {type}</p>
+      <p>Abilities: {abilities}</p>
       </div>
       </div>);
   }
@@ -52,7 +61,12 @@ getInitialState(){
           stat: "",
           id:'',
           pokemonname : "",
-          type : ""
+          type : "",
+          abilities : "",
+          sprite:"",
+          shiny:"",
+          isdisabled:"true"
+
           }
 },
 handleInput(name){
@@ -67,7 +81,9 @@ var url = "http://pokeapi.co/api/v2/pokemon/"+this.state.name;
       sprite:response.body.sprites.front_default,
       shiny:response.body.sprites.front_shiny,
       id:response.body.id,
-      type: response.body.types
+      type: response.body.types,
+      abilities:response.body.abilities,
+      isdisabled: "false"
     });
   });
 
@@ -85,13 +101,26 @@ var style = {
         <div className="App-header col-xs-12"><p> <img src = {head} className="head" /></p>
                </div>
              <div className="form-group leftclass  col-xs-4" style = {style}>
+             <h1 style = {{color:"black"}}> Search for a Pokemon</h1>
       <input value = {this.state.name}  className = "form-control" onChange = {this.handleInput} type = "text"/>
-      <button onClick = {this.handleClick} className = "btn btn-circle btn-lg btn-warning"> Search</button>
+
+      <button onClick = {this.handleClick} className = "btn btn-circle btn-lg btn-danger" style = {{marginTop:"3%"}}> Search</button>
+      <div className = "commentArea" style ={{color:"white"}}>
+      <h4>Pokedex Generation VI</h4><hr/>
+      <p>This is Pokedex Gen VI, a portable search engine for searching your favorite pokemon</p>
+      <p>Features:</p>
+      
+      - Search Engine<br/>
+      - Pokemon Description<br/>
+      - Comment Area <br/>
+      
+      </div>
       </div>
         <Detail name = {this.state.pokemonname} sprite = {this.state.sprite} 
-        weight = {this.state.stat} shiny = {this.state.shiny}
+        weight = {this.state.stat} shiny = {this.state.shiny} ability = {this.state.abilities}
         types = {this.state.type} id = {this.state.id}/>
-        <Comment/>
+        <Comment isdisabled = {this.state.disabled}/>
+
       </div>
     );
   }
