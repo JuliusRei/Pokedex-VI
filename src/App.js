318ca190ae915e696154ac01f805c77e03a85f8c
JuliusRei <morcillajr0912@gmail.com>
2016-11-28 16:47:14 +0800

    5th commit, can add comments

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import React from 'react';
 import head from './Pokemon.png';
 import './App.css';
 import Request from 'superagent';
+import axios from 'axios';
 var Comment = React.createClass({
-handleClick(){
- 
-},
+
   render(){
-    
+
     return(<div className="leftclass col-xs-4" >
       <h1  style={{color:"black"}}> Comment Area</h1>
-      <textarea className="form-control" style={{marginBottom: "1%"}}></textarea>
-      <button id="btnsave" className="btn btn-info" onClick={this.handleClick} >Leave a Comment</button>
+      <textarea onChange = {this.props.onTextChange} className="form-control" style={{marginBottom: "1%"}}></textarea>
+      <button onClick = {this.props.onSubmit} id="btnsave" className="btn btn-info"  >Leave a Comment</button>
       <div className="col-xs-12 commentArea" style={{color:"white"}} ></div>
       </div>);
   }
@@ -21,78 +20,73 @@ var Detail = React.createClass({
 
   render(){
     var border={
-  border: "3px solid black",
-  backgroundColor:"white",
-  paddingTop:"3px"
-};
-var abilities = this.props.ability;
-var type = this.props.types;
-      if(type !== ""){
-        type = this.props.types.map((n)=> {return "/"+n.type.name + "/"}); 
-        abilities = this.props.ability.map((n)=> {return "/"+n.ability.name + "/"});
-      }
-        else{
-          type = "-"; 
-          abilities = "-";         }
+      border: "3px solid black",
+      backgroundColor:"white",
+      paddingTop:"3px"
+    };
+    var abilities = this.props.ability;
+    var type = this.props.types;
+    if(type !== ""){
+      type = this.props.types.map((n)=> {return "/"+n.type.name + "/"}); 
+      abilities = this.props.ability.map((n)=> {return "/"+n.ability.name + "/"});
+    }
+    else{
+      type = "-"; 
+      abilities = "-";         }
 
-    return(
-          <div className="leftclass col-xs-4">
-          <h1 style={{color:"black"}}> Pokemon Descripton</h1>
-      <div className="form-group">
-      <div className="col-xs-6" style={border}>
-      <img alt="default" className="sprite" src={this.props.sprite}/><p>Original</p></div>
-      <div className="col-xs-6" style={border}>
-      <img alt="shiny" className="sprite" src={this.props.shiny}/><p>Shiny</p></div></div>
-      <div className="col-xs-12" style={border}>
-      <p>Pokemon ID: #{this.props.id}</p>
-      <p>Name: {this.props.name}</p>
-      <p>Height : {this.props.height}</p>
-      <p>Weight : {this.props.weight}</p>
-      <p>Type: {type}</p>
-      <p>Abilities: {abilities}</p>
-      </div>
-      </div>);
-  }
-});
+      return(
+        <div className="leftclass col-xs-4">
+        <h1 style={{color:"black"}}> Pokemon Descripton</h1>
+        <div className="form-group">
+        <div className="col-xs-6" style={border}>
+        <img alt="default" className="sprite" src={this.props.sprite}/><p>Original</p></div>
+        <div className="col-xs-6" style={border}>
+        <img alt="shiny" className="sprite" src={this.props.shiny}/><p>Shiny</p></div></div>
+        <div className="col-xs-12" style={border}>
+        <p>Pokemon ID: #{this.props.id}</p>
+        <p>Name: {this.props.name}</p>
+        <p>Height : {this.props.height}</p>
+        <p>Weight : {this.props.weight}</p>
+        <p>Type: {type}</p>
+        <p>Abilities: {abilities}</p>
+        </div>
+        </div>);
+    }
+  });
 
 
 
 var App = React.createClass({
-getInitialState(){
-  return {name : "",
-          stat: "",
-          id:'',
-          pokemonname : "",
-          type : "",
-          abilities : "",
-          sprite:"",
-          shiny:"",
-          isdisabled:"true",
-          stat2:""
-          }
+  getInitialState(){
+    return {name : "",
+    stat: "",
+    id:'',
+    pokemonname : "",
+    type : "",
+    abilities : "",
+    sprite:"",
+    shiny:"",
+    isdisabled:true,
+    stat2:"",
+    comment:"",
+    data:[]
+
+  }
 },
 handleInput(name){
   this.setState({name : name.target.value.toLowerCase()});
 },
 handleClick(){
-var url = "http://pokeapi.co/api/v2/pokemon/"+this.state.name;
+  var url = "http://pokeapi.co/api/v2/pokemon/"+this.state.name;
 
   Request.get(url)
   .end((err,response)=>{
-     if (err || !response.ok) {
-       alert(this.state.name + " cannot be found in our database");
-        this.setState({
-      pokemonname:"unknown",
-      stat:"unknown",
-      stat2:"unknown",
-      sprite:"",
-      shiny:"",
-      id:"unknown",
-      type: "",
-      abilities:"",
-      isdisabled: "false"
+   if (err || !response.ok) {
+     alert(err);
+     this.setState({
+      isdisabled: true
     });
-     } else {
+   } else {
     this.setState({
       pokemonname:response.body.name,
       stat:response.body.weight,
@@ -102,49 +96,70 @@ var url = "http://pokeapi.co/api/v2/pokemon/"+this.state.name;
       id:response.body.id,
       type: response.body.types,
       abilities:response.body.abilities,
-      isdisabled: "false"
+      isdisabled: false
     }); }
   })
-
-
-
+this.ReturnType();
 }, 
-  render() {
-var style = {
-      backgroundColor: 'transparent',
-     
-    };
+handleCommentChange(e){
+  this.setState({
+    comment : e.target.value
+  }); 
+},
+handleComment(){
 
-    return (
-      
-      <div className="App col-xs-12">
+  var url = 'http://localhost:3000/api/comments';
+Request.post(url)
+       .send({author: this.state.pokemonname,
+        text:this.state.comment,})
+       .end(alert('Comment Posted'))
+},
+ReturnType(){
+  var url = 'http://localhost:3000/api/comments';
+  Request.get(url)
+         .then((n)=>{
+          n.filter((response)=>{
+            response.author === this.state.pokemonname ? this.state.data.push(response) : null
+          })
+         });
+},
+render() {
+  var style = {
+    backgroundColor: 'transparent',
 
-        <div className="App-header col-xs-12"><p> <img src={head} alt="header" className="head" /></p>
-               </div>
-             <div className="form-group leftclass  col-xs-4" style={style}>
-             <h1 style={{color:"black"}}> Search for a Pokemon</h1>
-      <input value={this.state.name}  className="form-control" onChange={this.handleInput} type="text"/>
+  };
 
-      <button onClick={this.handleClick} className="btn btn-circle btn-lg btn-danger" style={{marginTop:"3%"}}> Search</button>
-      <div className="commentArea" style={{color:"white"}}>
-      <h4>Pokedex Generation VI</h4><hr/>
-      <p>This is Pokedex Gen VI, a portable search engine for searching your favorite pokemon</p>
-      <p>Features:</p>
-      
-      - Search Engine<br/>
-      - Pokemon Description<br/>
-      - Comment Area <br/>
-      
-      </div>
-      </div>
-        <Detail name={this.state.pokemonname} sprite={this.state.sprite} 
-        weight={this.state.stat} shiny={this.state.shiny} ability={this.state.abilities}
-        types={this.state.type} id={this.state.id} height={this.state.stat2}/>
-        <Comment isdisabled={this.state.disabled}/>
+  return (
 
-      </div>
+    <div className="App col-xs-12">
+
+    <div className="App-header col-xs-12"><p> <img src={head} alt="header" className="head" /></p>
+    </div>
+    <div className="form-group leftclass  col-xs-4" style={style}>
+    <h1 style={{color:"black"}}> Search for a Pokemon</h1>
+    <input value={this.state.name}  className="form-control" onChange={this.handleInput} type="text"/>
+
+    <button onClick={this.handleClick} className="btn btn-circle btn-lg btn-danger" style={{marginTop:"3%"}}> Search</button>
+    <div className="commentArea" style={{color:"white"}}>
+    <h4>Pokedex Generation VI</h4><hr/>
+    <p>This is Pokedex Gen VI, a portable search engine for searching your favorite pokemon</p>
+    <p>Features:</p>
+
+    - Search Engine<br/>
+    - Pokemon Description<br/>
+    - Comment Area <br/>
+
+    </div>
+    </div>
+    <Detail name={this.state.pokemonname} sprite={this.state.sprite} 
+            weight={this.state.stat} shiny={this.state.shiny} ability={this.state.abilities}
+            types={this.state.type} id={this.state.id} height={this.state.stat2}/>
+    {this.state.isdisabled ? null : <Comment onTextChange = {this.handleCommentChange}
+              onSubmit = {this.handleComment} />}
+
+    </div>
     );
-  }
+}
 });
 
 export default App;
